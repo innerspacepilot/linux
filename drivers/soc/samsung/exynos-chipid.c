0e75b2477303e5e56c7e774fce6867703cbbe47c
VDavid003 <virag.david003@gmail.com>
2020-11-23 12:07:49 +0100

    soc: samsung: chipid: Add exynos7885 soc id This patch adds Exynos 7885 soc id to the Samsung chipid driver.

--- drivers/soc/samsung/exynos-chipid.c
+++ drivers/soc/samsung/exynos-chipid.c
@@ -32,6 +32,7 @@ static const struct exynos_soc_id {
 	{ "EXYNOS5440", 0xE5440000 },
 	{ "EXYNOS5800", 0xE5422000 },
 	{ "EXYNOS7420", 0xE7420000 },
+	{ "EXYNOS7885", 0xE7885000 },
 	{ "EXYNOS5433", 0xE5433000 },
 };
 
